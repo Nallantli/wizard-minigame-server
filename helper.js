d076216f66371ecc69eda7d1bebb1080ed575852
Benjamin Park <parkbenjamin2@gmail.com>
2023-12-07 23:08:54 -0500

    initial commit

---
+++ helper.js
@@ -0,0 +1,153 @@
+export const spells = await (await fetch('https://raw.githubusercontent.com/Nallantli/wizard-minigame/master/spells.json')).json();
+
+function criticalChance(cra, crb) {
+	const diff = cra - crb;
+	return ((diff - 32) / (2 * (16 + Math.abs(diff - 32))) + 0.5) * (Math.min(cra, 100) / 100);
+}
+
+export function calculateDamages(spell, caster, victim) {
+	if (Math.random() > spell.chance) {
+		return 'FAILED';
+	}
+	if (spell.type !== 'ATTACK_ALL' && spell.type !== 'ATTACK_BASIC') {
+		return {};
+	}
+
+	const isCritical = Math.random() <= criticalChance(caster.entity.criticalRating, victim.entity.criticalRating);
+
+	const shields = victim.shields;
+	const blades = caster.blades;
+	let usedBladeIds = [];
+	let totalUsedBladeIds = [];
+	let baseTilt = 1;
+	blades.forEach(({ id, value, element }, i) => {
+		if ((spell.element === element || element === 'all') && !totalUsedBladeIds.includes(id)) {
+			baseTilt *= (value + 100) / 100;
+			totalUsedBladeIds.push(id);
+			usedBladeIds.push({ index: i, id });
+		}
+	});
+	let totalUsedShieldIds = [];
+	return {
+		isCritical,
+		usedBladeIds,
+		damages: spell.damages.map(d => {
+			let base = d.damage !== undefined ? d.damage : (Math.random() * (d.maxDamage - d.minDamage) + d.minDamage);
+			let usedShieldIds = [];
+			let currentElement = d.element;
+			shields.forEach(({ id, value, element, elementTo }, i) => {
+				if ((currentElement === element || element === 'all') && !totalUsedShieldIds.includes(id)) {
+					if (value) {
+						base += base * (value / 100);
+					}
+					if (elementTo) {
+						currentElement = elementTo;
+					}
+					usedShieldIds.push({ index: i, id });
+					totalUsedShieldIds.push(id);
+				}
+			});
+			if (isCritical) {
+				base *= 2;
+			}
+			let augment = victim.entity?.augments?.[currentElement];
+			return {
+				...d,
+				damage: Math.round(base * baseTilt * (augment !== undefined ? augment : 1)),
+				augmented: augment !== undefined ? (augment > 1 ? ' ++' : ' --') : undefined,
+				usedShieldIds
+			};
+		})
+	};
+}
+
+export function iterateSpell(casterIndex, victimIndices, spellIndex, battleData, calculatedDamages) {
+	if (calculatedDamages.length === 1 && calculatedDamages[0] === 'FAILED') {
+		const spellId = battleData[casterIndex].hand[spellIndex];
+		battleData[casterIndex].deck = [
+			spellId,
+			...battleData[casterIndex].deck
+		];
+		battleData[casterIndex].hand.splice(spellIndex, 1);
+		return battleData;
+	}
+	const spell = spells[battleData[casterIndex].hand[spellIndex]];
+	switch (spell.type) {
+		case 'HEALING_BASIC':
+			spell.heals.forEach(({ heal }) => battleData[victimIndices[0]].entity.health += heal);
+			break;
+		case 'ATTACK_BASIC':
+		case 'ATTACK_ALL':
+			{
+				victimIndices.forEach((victimIndex, i) => {
+					let newShields = [];
+					const { usedBladeIds, damages } = calculatedDamages[i];
+					damages.forEach(({ damage, usedShieldIds, steal }) => {
+						for (let j = 0; j < battleData[victimIndex].shields.length; j++) {
+							if (!usedShieldIds.map(({ index }) => index).includes(j)) {
+								newShields.push(battleData[victimIndex].shields[j]);
+							}
+						}
+						battleData[victimIndex].entity.health += damage;
+						if (steal) {
+							battleData[casterIndex].entity.health -= damage * steal;
+						}
+					});
+					battleData[casterIndex].blades = battleData[casterIndex].blades.filter((_, j) => !usedBladeIds.map(({ index }) => index).includes(j));
+					battleData[victimIndex].shields = newShields;
+				});
+			}
+			break;
+	}
+	if (spell.victimShields) {
+		victimIndices.forEach(i => {
+			battleData[i].shields = [
+				...battleData[i].shields,
+				...spell.victimShields
+			];
+		});
+	}
+	if (spell.casterShields) {
+		battleData[casterIndex].shields = [
+			...battleData[casterIndex].shields,
+			...spell.casterShields
+		];
+	}
+	if (spell.victimBlades) {
+		victimIndices.forEach(i => {
+			battleData[i].blades = [
+				...battleData[i].blades,
+				...spell.victimBlades
+			];
+		});
+	}
+	if (spell.casterBlades) {
+		battleData[casterIndex].blades = [
+			...battleData[casterIndex].blades,
+			...spell.casterBlades
+		];
+	}
+
+	let vrilLeft = spell.vrilRequired;
+	while (vrilLeft > 1 && battleData[casterIndex].superVril > 0) {
+		battleData[casterIndex].superVril--;
+		vrilLeft -= 2;
+	}
+	if (battleData[casterIndex].vril === 0) {
+		battleData[casterIndex].superVril -= Math.ceil(vrilLeft / 2);
+	} else {
+		battleData[casterIndex].vril -= vrilLeft;
+	}
+	battleData[casterIndex].hand.splice(spellIndex, 1);
+
+	for (let i = 0; i < battleData.length; i++) {
+		if (battleData[i] === null) {
+			continue;
+		}
+		battleData[i].entity.health = Math.round(battleData[i].entity.health);
+		if (battleData[i].entity.health > battleData[i].entity.maxHealth) {
+			battleData[i].entity.health = battleData[i].entity.maxHealth;
+		}
+	}
+	return battleData;
+}
---
+++ index.js
@@ -0,0 +1,430 @@
+import { WebSocketServer } from 'ws';
+import * as https from 'https';
+import * as fs from 'fs';
+
+import { calculateDamages, iterateSpell, spells } from './helper.js';
+
+let runningGames = {};
+
+const ID_CHARS = [
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+	'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+];
+
+function randomFromList(l) {
+	return l[Math.floor(Math.random() * l.length)];
+}
+
+function generateId() {
+	let id = '';
+	for (let i = 0; i < 4; i++) {
+		id += randomFromList(ID_CHARS);
+	}
+	return id;
+}
+
+function createGame(ws, entity) {
+	let id = generateId();
+	while (Object.keys(runningGames).includes(id)) {
+		id = generateId();
+	}
+
+	const turnState = {
+		battleData: [generateBattleEntity(entity, false), null, null, null, null, null, null, null],
+		selectedCards: [null, null, null, null, null, null, null, null],
+		selectedVictims: [[], [], [], [], [], [], [], []],
+		battleIndex: -2
+	};
+
+	runningGames[id] = { id, turnState, sockets: [{ ws, pos: 0, isReady: false }] };
+
+	return id;
+}
+
+function propagateState(id) {
+	const { sockets, turnState } = runningGames[id];
+	sockets.forEach(({ ws, pos }) => ws.send(JSON.stringify({
+		action: 'STATE_UPDATE',
+		turnState,
+		id,
+		playerIndex: pos,
+		players: sockets.map(({ pos, isReady }) => ({ pos, isReady }))
+	})));
+}
+
+function joinGame(id, ws, entity) {
+	if (gameExists(id) && runningGames[id].turnState.battleIndex === -2) {
+		const nextEmptyPos = runningGames[id].turnState.battleData.findIndex(e => e === null);
+		runningGames[id].sockets.push({ ws, pos: nextEmptyPos, isReady: false });
+		runningGames[id].turnState.battleData[nextEmptyPos] = generateBattleEntity(entity, false);
+		return true;
+	}
+	return false;
+}
+
+function socketIsConnectedToGame(socket, id) {
+	return runningGames[id].sockets.find(({ ws }) => socket === ws) !== undefined;
+}
+
+function gameExists(id) {
+	return runningGames[id] !== undefined;
+}
+
+function checkWin(id) {
+	const { turnState, sockets, leftStart, rightStart } = runningGames[id];
+	if (turnState.battleData.filter((e, i) => i < 4 && e !== null).length === 0) {
+		runningGames[id].turnState.battleIndex = -3;
+		sockets.forEach(({ ws, pos }) => ws.send(JSON.stringify({
+			action: 'WIN',
+			side: 'RIGHT',
+			entities: rightStart.map(({ entity }) => entity)
+		})));
+	} else if (turnState.battleData.filter((e, i) => i >= 4 && e !== null).length === 0) {
+		runningGames[id].turnState.battleIndex = -3;
+		sockets.forEach(({ ws, pos }) => ws.send(JSON.stringify({
+			action: 'WIN',
+			side: 'LEFT',
+			entities: leftStart.map(({ entity }) => entity)
+		})));
+	}
+}
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+	for (var i = array.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+}
+
+function generateBattleEntity(entity, isAI) {
+	const hasSuperVril = Math.random() <= entity.superVrilChance;
+
+	return {
+		shields: [],
+		blades: [],
+		vril: hasSuperVril ? 0 : 1,
+		superVril: hasSuperVril ? 1 : 0,
+		entity,
+		isAI
+	}
+}
+
+function setDeck(id, pos, deck) {
+	runningGames[id].turnState.battleData[pos].deck = deck;
+}
+
+function doRound(id) {
+	let animationData = [];
+	for (let i = 0; i < 8; i++) {
+		runningGames[id].turnState.battleIndex = i;
+		const victimIndices = runningGames[id].turnState.selectedVictims[runningGames[id].turnState.battleIndex]
+			.filter(i => runningGames[id].turnState.battleData[i] !== null && runningGames[id].turnState.battleData[i].entity.health > 0);
+		if (victimIndices.length > 0 && runningGames[id].turnState.battleData[runningGames[id].turnState.battleIndex].entity.health > 0) {
+			const spellIndex = runningGames[id].turnState.selectedCards[runningGames[id].turnState.battleIndex];
+			const spell = spells[runningGames[id].turnState.battleData[runningGames[id].turnState.battleIndex].hand[spellIndex]];
+			const calculatedDamages = victimIndices
+				.map(i => runningGames[id].turnState.battleData[i])
+				.map(victimData => calculateDamages(
+					spell,
+					runningGames[id].turnState.battleData[runningGames[id].turnState.battleIndex],
+					victimData));
+			animationData.push({
+				victimIndices: victimIndices.slice().reverse(),
+				spell,
+				calculatedDamages: calculatedDamages.slice().reverse(),
+				turnState: JSON.parse(JSON.stringify(runningGames[id].turnState))
+			});
+			runningGames[id].turnState.battleData = iterateSpell(runningGames[id].turnState.battleIndex, victimIndices, spellIndex, runningGames[id].turnState.battleData, calculatedDamages);
+		}
+	}
+
+	for (let i = 0; i < runningGames[id].turnState.battleData.length; i++) {
+		if (runningGames[id].turnState.battleData[i] && runningGames[id].turnState.battleData[i].entity.health > 0) {
+			if (Math.random() <= runningGames[id].turnState.battleData[i].superVrilChance) {
+				runningGames[id].turnState.battleData[i].superVril++;
+			} else {
+				runningGames[id].turnState.battleData[i].vril++;
+			}
+		} else if (runningGames[id].turnState.battleData[i] && runningGames[id].turnState.battleData[i].entity.health <= 0) {
+			runningGames[id].turnState.battleData[i] = null;
+		}
+	}
+	for (let i = 0; i < runningGames[id].turnState.battleData.length; i++) {
+		if (!runningGames[id].turnState.battleData[i]) {
+			continue;
+		}
+		while (runningGames[id].turnState.battleData[i].hand.length < 7) {
+			const cardId = runningGames[id].turnState.battleData[i].deck.pop();
+			if (!cardId) {
+				break;
+			}
+			runningGames[id].turnState.battleData[i].hand.push(cardId);
+		}
+	}
+
+	runningGames[id].turnState = {
+		...runningGames[id].turnState,
+		battleIndex: -1,
+		selectedCards: [null, null, null, null, null, null, null, null],
+		selectedVictims: [[], [], [], [], [], [], [], []],
+	}
+
+	return {
+		animationData,
+		finalTurnState: runningGames[id].turnState
+	};
+}
+
+function shoveDownEntity(id, pos) {
+	const limitPos = pos >= 4 ? 4 : 0;
+	const { turnState, sockets } = runningGames[id];
+	const socketIndex = sockets.findIndex(e => e.pos === pos);
+	while (pos > limitPos && turnState.battleData[pos - 1] === null) {
+		runningGames[id].turnState.battleData[pos - 1] = runningGames[id].turnState.battleData[pos];
+		runningGames[id].turnState.battleData[pos] = null;
+		pos--;
+	}
+	sockets[socketIndex].pos = pos;
+}
+
+function startGame(id) {
+	unreadyAll(id);
+	runningGames[id].turnState.battleIndex = -1;
+	runningGames[id].leftStart = [];
+	runningGames[id].rightStart = [];
+
+	for (let i = 0; i < runningGames[id].turnState.battleData.length; i++) {
+		if (runningGames[id].turnState.battleData[i] === null) {
+			continue;
+		}
+
+		if (i < 4) {
+			runningGames[id].leftStart.push({ ws: runningGames[id].sockets.find(({ pos }) => pos === i).ws, entity: runningGames[id].turnState.battleData[i].entity });
+		} else if (i >= 4) {
+			runningGames[id].rightStart.push({ ws: runningGames[id].sockets.find(({ pos }) => pos === i).ws, entity: runningGames[id].turnState.battleData[i].entity });
+		}
+
+		const deck = runningGames[id].turnState.battleData[i].deck
+		shuffleArray(deck);
+
+		let hand = [];
+		for (let i = 0; i < 7; i++) {
+			const card = deck.pop();
+			if (!card) {
+				break;
+			}
+			hand.push(card);
+		}
+		runningGames[id].turnState.battleData[i].hand = hand;
+	}
+}
+
+function areAllPlayersReady(id) {
+	const { turnState } = runningGames[id];
+	for (let i = 0; i < turnState.battleData.length; i++) {
+		if (turnState.battleData[i] === null) {
+			continue;
+		}
+		if (turnState.selectedCards[i] === null || (turnState.selectedCards[i] !== 'PASS' && turnState.selectedVictims[i].length === 0)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function startBattleSequence(id) {
+	runningGames[id].turnState.battleIndex = 0;
+	propagateState(id);
+	const animationData = doRound(id);
+	runningGames[id].sockets.forEach(({ ws }) => ws.send(JSON.stringify({
+		action: 'BATTLE_ANIMATION_DATA',
+		...animationData
+	})));
+	checkWin(id);
+}
+
+function unreadyAll(id) {
+	runningGames[id].sockets.forEach(e => e.isReady = false);
+}
+
+function basicCheck(ws, id) {
+	if (!gameExists(id)) {
+		ws.send(JSON.stringify({
+			action: "MOVE_SELF_FAILURE",
+			message: `Game '${id}' does not exist`
+		}));
+		return false;
+	}
+	if (!socketIsConnectedToGame(ws, id)) {
+		ws.send(JSON.stringify({
+			action: "MOVE_SELF_FAILURE",
+			message: `Socket is not connected to game '${id}'`
+		}));
+		return false;
+	}
+	return true;
+}
+
+const server = https.createServer({
+	cert: fs.readFileSync('/etc/letsencrypt/live/wmgs.nallant.li/fullchain.pem'),
+	key: fs.readFileSync('/etc/letsencrypt/live/wmgs.nallant.li/privkey.pem')
+});
+
+server.listen(8080);
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+	ws.on('close', (code, desc) => {
+		console.log(`${new Date(Date.now()).toISOString()} | Closed connection`, code, desc);
+		const game = Object.entries(runningGames).find(([_, value]) => {
+			if (value && value.sockets.find(e => e.ws === ws)) {
+				return true;
+			}
+			return false;
+		});
+		if (game) {
+			const id = game[0];
+			const wsIndex = runningGames[id].sockets.findIndex(e => e.ws === ws);
+			runningGames[id].turnState.battleData[runningGames[id].sockets[wsIndex].pos] = null;
+			runningGames[id].sockets.splice(wsIndex, 1);
+			if (runningGames[id].sockets.length === 0) {
+				console.log(`${new Date(Date.now()).toISOString()} | Closing game ${id} (Total Current Games: ${Object.entries(runningGames).length - 1})`);
+				delete runningGames[id];
+			} else {
+				if (runningGames[id].turnState.battleIndex === -2) {
+					for (let i = 0; i < 8; i++) {
+						if (runningGames[id].turnState.battleData[i] !== null) {
+							shoveDownEntity(id, i);
+						}
+					}
+				}
+				propagateState(id);
+				if (runningGames[id].turnState.battleIndex > -2) {
+					checkWin(id);
+				}
+			}
+		}
+	});
+	ws.on('message', (raw) => {
+		const data = JSON.parse(raw);
+		switch (data.action) {
+			case 'CREATE_GAME': {
+				const { entity } = data;
+				const id = createGame(ws, entity);
+				console.log(`${new Date(Date.now()).toISOString()} | Created Game: ${id} (Total Current Games: ${Object.entries(runningGames).length})`);
+				propagateState(id);
+				break;
+			}
+			case 'JOIN_GAME': {
+				const { id, entity } = data;
+				if (joinGame(id, ws, entity)) {
+					unreadyAll(id);
+					propagateState(id);
+				} else {
+					ws.send(JSON.stringify({
+						action: "JOIN_FAILURE",
+						message: `Cannot join game '${id}'`
+					}));
+					return;
+				}
+				break;
+			}
+			case 'MOVE_SELF': {
+				const { id, pos } = data;
+				if (!basicCheck(ws, id)) {
+					return;
+				}
+				if (runningGames[id].turnState.battleData[pos] !== null) {
+					ws.send(JSON.stringify({
+						action: "MOVE_SELF_FAILURE",
+						message: `Position ${pos} is already occupied`
+					}));
+					return;
+				}
+				const oldPos = runningGames[id].sockets.find(e => e.ws === ws).pos;
+				runningGames[id].sockets.find(e => e.ws === ws).pos = pos;
+				runningGames[id].turnState.battleData[pos] = runningGames[id].turnState.battleData[oldPos];
+				runningGames[id].turnState.battleData[oldPos] = null;
+				for (let i = 0; i < 8; i++) {
+					if (runningGames[id].turnState.battleData[i] !== null) {
+						shoveDownEntity(id, i);
+					}
+				}
+				unreadyAll(id);
+				propagateState(id);
+				break;
+			}
+			case 'READY_UP': {
+				const { id, deck } = data;
+				if (!basicCheck(ws, id)) {
+					return;
+				}
+				runningGames[id].sockets.find(e => e.ws === ws).isReady = true;
+				setDeck(id, runningGames[id].sockets.find(e => e.ws === ws).pos, deck);
+				if (runningGames[id].sockets.find(e => !e.isReady) === undefined) {
+					startGame(id);
+				}
+				propagateState(id);
+				break;
+			}
+			case 'READY_DOWN': {
+				const { id } = data;
+				if (!basicCheck(ws, id)) {
+					return;
+				}
+				runningGames[id].sockets.find(e => e.ws === ws).isReady = false;
+				propagateState(id);
+				break;
+			}
+			case 'SELECT_CARD': {
+				const { id, card } = data;
+				if (!basicCheck(ws, id)) {
+					return;
+				}
+				const playerIndex = runningGames[id].sockets.find(e => e.ws === ws).pos;
+				runningGames[id].turnState.selectedCards[playerIndex] = card;
+				if (areAllPlayersReady(id)) {
+					startBattleSequence(id);
+				} else {
+					propagateState(id);
+				}
+				break;
+			}
+			case 'SELECT_VICTIMS': {
+				const { id, victims } = data;
+				if (!basicCheck(ws, id)) {
+					return;
+				}
+				const playerIndex = runningGames[id].sockets.find(e => e.ws === ws).pos;
+				runningGames[id].turnState.selectedVictims[playerIndex] = victims;
+				if (areAllPlayersReady(id)) {
+					startBattleSequence(id);
+				} else {
+					propagateState(id);
+				}
+				break;
+			}
+			case 'DISCARD_CARD': {
+				const { id, hand } = data;
+				if (!basicCheck(ws, id)) {
+					return;
+				}
+				const playerIndex = runningGames[id].sockets.find(e => e.ws === ws).pos;
+				runningGames[id].turnState.battleData[playerIndex].hand = hand;
+				propagateState(id);
+				break;
+			}
+		}
+	});
+
+	ws.send(JSON.stringify({
+		action: 'SUCCESS',
+		message: 'Connected'
+	}), ws);
+});
+
+console.log('Created WebSocket');
